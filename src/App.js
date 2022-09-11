86f288bf55b7b58c8c5ebf287bff760007a8b669
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-11 12:39:39 +0600

    client side setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AddProduct from './components/AddProduct/AddProduct';
+import Home from './components/Home/Home';
+import ProductList from './components/ProductList/ProductList';
 
 function App() {
   return (
     <div>
-      <h2>hello pagination </h2>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/addproduct' element={<AddProduct></AddProduct>}></Route>
+        <Route path='/productlist' element={<ProductList></ProductList>}></Route>
+
+      </Routes>
     </div>
   );
 }
